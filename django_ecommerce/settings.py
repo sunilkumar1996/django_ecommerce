4c18d7445212045ee3d8c23d71f89d81891177a5
sunilkumar2020 <sunilrajputdev@gmail.com>
2021-06-24 18:15:19 +0530

    custom user model add

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+AUTH_USER_MODEL = 'user.CustomUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
